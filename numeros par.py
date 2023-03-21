ed3808e1479c644d49cff3894b573e5839de51e8
Kevincas10 <125384923+Kevincas10@users.noreply.github.com>
2023-03-21 09:25:34 -0600

    Se creo el archivo comparar numeros.py
    para buscar que numero es par y que numero es impar.

---
+++ numeros par.py
@@ -0,0 +1,10 @@
+import sys
+b=int(input("Ingrese el valor a comparar si es par: "))
+
+if b % 2 == 0:
+    print('El número', b, 'es par.')
+else:
+    print('El número', b, 'es impar.')    
+    
+    
+    
